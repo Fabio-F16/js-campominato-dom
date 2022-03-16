6eef9957e118d66d51e31cef940acbfa7f6d187e
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-16 18:12:18 +0100

    bombe

--- script.js
+++ script.js
@@ -7,7 +7,102 @@ Tre difficoltà:
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49 
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+
+
 */
+
+
+// -----------------------------------------funzioni
+
+
+
+
+
+
+// genero un numero random (non utilizzata)
+function generateRandomNumber(min, max) {
+    const range = (max - min) + 1;
+    const numeroRandom = Math.floor(Math.random() * range + min);
+    return numeroRandom;
+}
+// fine genero un numero random (non utilizzata)
+
+
+
+
+// creo una cella nel DOM
+function createCell() {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    return cell;
+}
+// fine creo una cella nel DOM
+
+
+// funzione creazione griglia
+function creazioneCampo(totalCells, levelClass) {
+
+    const square = document.getElementById('square');
+    square.innerHTML = '';
+    const arrayBombe = createBombs(totalCells);
+
+    for (let i = 1; i <= totalCells; i++) {
+
+        const cell = createCell();
+        cell.classList.add(levelClass);
+
+        let number = i
+        cell.innerText = number;
+        square.appendChild(cell);
+
+        cell.addEventListener('click', () => {
+
+            const leBombe = arrayBombe.includes(i);
+
+            if (leBombe) {
+                cell.classList.add('bg-color-red');
+                alert('Hai perso!')
+            } else {
+                cell.classList.add('bg-color-aqua');
+            }
+        })
+
+    }
+}
+// fine funzione creazione griglia
+
+
+
+// creazione bombe
+function createBombs(max) {
+
+    // creo un array vuoto dove inserire le posizioni
+    const arrayBombs = [];
+
+    // creo un ciclo per creare massimo 16 numeri
+    while (arrayBombs.length < 16) {
+
+        //creo la posizione random 
+        const number = generateRandomNumber(1, max)
+
+        // se position non è presente all'interno dell'array, lo aggiungo
+        if (!arrayBombs.includes(number)) {
+            arrayBombs.push(number);
+        }
+    }
+    console.log(arrayBombs)
+
+    return arrayBombs;
+}
+// fine creazione bombe
+
+// -----------------------------------------------------fine funzioni
+
+
+
 
 // chiamo bottoni genera griglia
 const difficoltàUno = document.querySelector('.difficoltàUno');
@@ -24,44 +119,5 @@ difficoltàTre.addEventListener('click', () => creazioneCampo(49, 'cell-7'));
 
 
 
-// funzioni
 
-// genero un numero random (non utilizzata)
-function generateRandomNumber(min, max) {
-    const range = (max - min) + 1;
-    const numeroRandom = Math.floor(Math.random() * range + min);
-    return numeroRandom;
-}
-// fine genero un numero random (non utilizzata)
 
-// creo una cella nel DOM
-function createCell() {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    return cell;
-}
-// fine creo una cella nel DOM
-
-// funzione creazione griglia
-function creazioneCampo(totalCells, levelClass) {
-
-    const square = document.getElementById('square');
-    square.innerHTML = '';
-
-    for (let i = 1; i <= totalCells; i++) {
-
-        const cell = createCell();
-        cell.classList.add(levelClass);
-
-        let number = i
-        cell.innerText = number;
-        square.appendChild(cell);
-
-        cell.addEventListener('click', () => cell.classList.toggle('bg-color-aqua'));
-
-    }
-
-}
-// fine funzione creazione griglia
-
-// fine funzioni
